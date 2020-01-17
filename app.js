4d11472150db826b254beaf3c90cbf819b5b647a
huycan511 <nguyenquanghuy0511@gmail.com>
2020-01-17 10:28:06 +0700

    add data to firestore db

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
+
 db.collection('cafes').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         renderList(doc); 
     });
+});
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value,
+    })
+    form.name.value = '';
+    form.city.value = '';
 });
 
 const renderList = doc => {
